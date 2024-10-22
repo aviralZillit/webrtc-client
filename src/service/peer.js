ed665d9db142c1659c81527f9ecae04f0a5363ac
AviralZillit <147372963+aviralZillit@users.noreply.github.com>
2024-10-23 00:35:22 +0530

    fix media stream

--- src/service/peer.js
+++ src/service/peer.js
@@ -1,21 +1,51 @@
 class PeerService {
   constructor() {
-    if (!this.peer) {
-      this.peer = new RTCPeerConnection({
-        iceServers: [
-          {
-            urls: [
-              "stun:stun.l.google.com:19302",
-              "stun:global.stun.twilio.com:3478",
-            ],
-          },
-        ],
-      });
-    }
+    this.peer = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:global.stun.twilio.com:3478",
+          ],
+        },
+      ],
+    });
+
+    this.localStream = null;
+    this.remoteStream = new MediaStream();
+
+    this.peer.ontrack = (event) => {
+      // Avoid adding duplicate tracks to the remote stream
+      if (!this.remoteStream.getTracks().includes(event.track)) {
+        this.remoteStream.addTrack(event.track);
+        console.log("Remote track added:", event.track);
+      }
+    };
+  }
+
+  async addLocalStream(stream) {
+    this.localStream = stream;
+
+    // Add tracks if they haven't been added yet
+    this.localStream.getTracks().forEach((track) => {
+      const existingSenders = this.peer.getSenders();
+      const trackExists = existingSenders.some(
+        (sender) => sender.track === track
+      );
+
+      if (!trackExists) {
+        this.peer.addTrack(track, this.localStream);
+        console.log("Local track added:", track);
+      }
+    });
   }
 
   async getAnswer(offer) {
     if (this.peer) {
+      if (this.peer.signalingState === "stable") {
+        console.warn("Peer is already stable. Ignoring remote offer.");
+        return;
+      }
       await this.peer.setRemoteDescription(offer);
       const ans = await this.peer.createAnswer();
       await this.peer.setLocalDescription(new RTCSessionDescription(ans));
@@ -25,6 +55,10 @@ class PeerService {
 
   async setLocalDescription(ans) {
     if (this.peer) {
+      if (this.peer.signalingState !== "have-remote-offer") {
+        console.warn("Peer is not expecting an answer. Ignoring remote answer.");
+        return;
+      }
       await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
     }
   }
@@ -35,6 +69,10 @@ class PeerService {
       await this.peer.setLocalDescription(new RTCSessionDescription(offer));
       return offer;
     }
+  }
+
+  getRemoteStream() {
+    return this.remoteStream;
   }
 }
 
