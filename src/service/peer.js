10098fcf0998fba580a34d73822ba5ef1a99e70f
AviralZillit <147372963+aviralZillit@users.noreply.github.com>
2024-11-06 17:02:51 +0530

    added changes in peer.js

--- src/service/peer.js
+++ src/service/peer.js
@@ -16,39 +16,24 @@ class PeerService {
 
   async getAnswer(offer) {
     if (this.peer) {
-      // Ensure the peer connection is ready for setting the remote offer
-      if (this.peer.signalingState === "stable" || this.peer.signalingState === "have-local-offer") {
-        await this.peer.setRemoteDescription(offer);
-        const ans = await this.peer.createAnswer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(ans));
-        return ans;
-      } else {
-        console.error("Cannot set remote offer in current signaling state:", this.peer.signalingState);
-      }
+      await this.peer.setRemoteDescription(offer);
+      const ans = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(ans));
+      return ans;
     }
   }
 
   async setLocalDescription(ans) {
     if (this.peer) {
-      // Check if the connection is in the correct state to set a remote answer
-      if (this.peer.signalingState === "have-remote-offer") {
-        await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
-      } else {
-        console.error("Cannot set remote answer in current signaling state:", this.peer.signalingState);
-      }
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ans));
     }
   }
 
   async getOffer() {
     if (this.peer) {
-      // Create an offer only if the peer connection is in a stable state
-      if (this.peer.signalingState === "stable") {
-        const offer = await this.peer.createOffer();
-        await this.peer.setLocalDescription(new RTCSessionDescription(offer));
-        return offer;
-      } else {
-        console.error("Cannot create offer in current signaling state:", this.peer.signalingState);
-      }
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
     }
   }
 }
